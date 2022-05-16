65a3d31ceb577353aab15c3bb53902fa2e216e62
Emerson Ferreira <emersonfdasilva2@gmail.com>
2022-05-15 21:39:22 -0300

    Projeto criado

---
+++ src/test/java/io/github/emfsilva/curso/modelagem/conceitual/CursoModelagemConceitualApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.emfsilva.curso.modelagem.conceitual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoModelagemConceitualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
